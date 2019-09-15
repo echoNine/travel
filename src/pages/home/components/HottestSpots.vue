8d92eff54f6e068584948427fa225ad3eef2e577
yat <2208839499@qq.com>
2019-09-15 14:27:13 +0800

    '首页index基本完成+ajax数据传递'

--- src/pages/home/components/HottestSpots.vue
+++ src/pages/home/components/HottestSpots.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="title">火爆景点</div>
     <ul>
-      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+      <li class="item border-bottom" v-for="item of list" :key="item.id">
         <div class="item-img-wrapper">
           <img class="item-img" :src="item.imgUrl" />
         </div>
@@ -18,20 +18,8 @@
 <script>
 export default {
   name: 'HomeHottestSpots',
-  data () {
-    return {
-      recommendList: [{
-        id: '0001',
-        imgUrl: '//img1.qunarzz.com/sight/p0/1909/11/11482fe19c41b2efa3.water.jpg_256x160_2a317c55.jpg',
-        title: '南浔古镇',
-        desc: '水乡古镇九十九，不如南浔走一走'
-      }, {
-        id: '0002',
-        imgUrl: '//img1.qunarzz.com/sight/p0/201405/27/8211cadd5d9fefeaa044c5b7eefc695d.jpg_256x160_5d730aee.jpg',
-        title: '千岛湖',
-        desc: '亲近大自然，放飞心情'
-      }]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
@@ -41,7 +29,6 @@ export default {
   .title
     line-height .8rem
     background #eee
-    margin-top .2rem
     text-indent .2rem
     font-size .3rem
   .item-img-wrapper
