81e6564fc7164753c77d4d3015e7ce57b08f07d4
yat <2208839499@qq.com>
2019-09-15 18:20:36 +0800

    City 当前城市、热门城市、城市列表及滚动A B..布局实现

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="btn-list">
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div>
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div>
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div>
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="btn-list">
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div>
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div>
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div>
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div><div class="btn-wrapper">
+            <div class="btn">北京</div>
+         </div>
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div>
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div>
+          <div class="btn-wrapper">
+            <div class="btn">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted: function () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+  .border-bottom
+    &:before
+      border-color #ccc
+  .list
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    .title
+      line-height .54rem
+      background #eee
+      padding-left .2rem
+      color #666
+      font-size .28rem
+    .btn-list
+      overflow hidden
+      padding .1rem .6rem .1rem .1rem
+      .btn-wrapper
+        float left
+        width 33.33%
+        .btn
+          text-align center
+          margin .1rem
+          border .02rem solid #ccc
+          padding .1rem 0
+          border-radius .06rem
+    .item-list
+      line-height .76rem
+      padding-left .2rem
+</style>
