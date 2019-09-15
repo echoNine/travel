68c287ee521648d4da556e30daaf8f6844ca531b
yat <2208839499@qq.com>
2019-09-15 15:35:08 +0800

    'index-router分支创建'

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">城市选择
+    <router-link to='/'>
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~sty/varibles.styl"
+  .header
+    overflow: hidden
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #ff
+    background $bgColor
+    font-size .32rem
+    position relative
+    .header-back
+      width .64rem
+      font-size .4rem
+      text-align center
+      position absolute
+      top 0
+      left 0
+      color #fff
+</style>
